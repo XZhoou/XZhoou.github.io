69c6109603ec8c21d7ee8d58dc49660b4e82c0a0
sunxzhoou <buaasqh@163.com>
2026-04-02 14:04:33 +0800

    Fix PDF filenames and workflow settings

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,9 @@
 @echo off
 setlocal
 
-:: Get current date and time for the commit message
-set datetime=%date% %time%
+:: Get current date and time for the commit message in YYYY-MM-DD HH:MM:SS format
+for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set dt=%%I
+set datetime=%dt:~0,4%-%dt:~4,2%-%dt:~6,2% %dt:~8,2%:%dt:~10,2%:%dt:~12,2%
 
 echo Updating site at %datetime%...
 
